06273aa08247c113272809b8c2c8f47a4a3fb950
Daniil-Vlasenko <daniil_vlasenko01@mail.ru>
2021-03-01 09:46:44 +0300

    Исправил блок contacts
    По дефолту в js он задавался block, поэтому отображался при открытии странички. Теперь это не так

--- myscript.js
+++ myscript.js
@@ -1,15 +1,8 @@
 var btn_contacts = document.querySelector('a.contacts_bytton');
 var contacts = document.querySelector("div.contacts");
-contacts.style.display = "block"; 
 
-// function noneOrBlock()
  
 btn_contacts.addEventListener("click", function(){          
-    // contacts.style.display = (contacts.style.display == "block") ? "none" : "block"; 
-    if (contacts.style.display == "block"){
-        contacts.style.display = "none";
-    } else{
-        contacts.style.display = "block";        
-    }    
+    contacts.style.display = (contacts.style.display == "block") ? "none" : "block";   
 });
 
